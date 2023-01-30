599160732544af554325dfcf55788b0fb03324c8
jxes6102 <jxes6102@gamil.com>
2023-01-30 10:37:31 +0800

    temp01301040

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
+      <NuxtLayout name="default">
+        <template #header>
+          <p class="px-6 pt-4 text-xl text-green-500">這段會放置在 header 插槽</p>
+        </template>
+        <template #default>
+          <p class="px-6 pt-4 text-xl text-cyan-500">
+            被 NuxtLayout 包裹的元件將會放置到 Layout 的預設 slot(default) 中
+          </p>
+        </template>
+        <template #footer>
+          <p class="px-6 pt-4 text-xl text-blue-500">這段會放置在 footer 插槽</p>
+        </template>
+      </NuxtLayout>
       <NuxtPage />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,19 @@
     <!-- <NuxtWelcome /> -->
     <app-test></app-test>
     <div class="mine-flex-center">
-        <p class="mt-4 text-9xl font-bold text-gray-900">is index</p>
+        <p class="mt-4 text-2xl font-bold text-gray-900">is index</p>
     </div>
     <div class="mine-flex-center gap-4">
         <NuxtLink to="/page1">前往 page1</NuxtLink>
         <NuxtLink to="/page2">前往 page2</NuxtLink>
         <NuxtLink to="/page3">前往 page3</NuxtLink>
     </div>
+    <!-- <div class="mine-flex-center">{{ $route.params }}</div> -->
 </div>
 </template>
 <script lang="ts" setup>
+// console.log('a')
+// const route = useRoute()
+// const { id } = route.params
+// console.log('b')
 </script>
