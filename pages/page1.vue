71946de0e7f76637e24a23a10728211af4b9d05b
jxes6102 <jxes6102@gamil.com>
2023-01-30 11:05:42 +0800

    temp01301106

--- pages/page1.vue
+++ pages/page1.vue
@@ -1,9 +1,22 @@
 <template>
     <div>
-        <div class="mine-flex-center">
-            <p class="mt-4 text-9xl font-bold text-gray-900">is page1</p>
+        <div class="mine-flex-center gap-5">
+            <p class="mt-4 text-2xl font-bold text-gray-900">is page1</p>
+            <button @click="changeStatus" type="submit" class="mt-6 bg-blue-600 py-3 px-8 text-xl font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">動態元件開關</button>
         </div>
+        <component :is="show ? DynamicComponent : 'div'" />
     </div>
 </template>
 <script lang="ts" setup>
+
+// function changeStatus(): void {
+//     show.value = !show.value
+// }
+
+const show = ref(false)
+const DynamicComponent = resolveComponent('NuxtWelcome')
+const changeStatus = function (): void {
+    show.value = !show.value
+    console.log('dodo')
+}
 </script>
