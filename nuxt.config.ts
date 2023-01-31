327c53ad60bb9e15510bea93849ed5073b771f13
jxes6102 <jxes6102@gamil.com>
2023-01-31 11:48:26 +0800

    temp01311150

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
         // 部署至githubpage需設定為專案名稱
         baseURL: '/nuxt3App/',
         // // 設定打包後資料夾名稱
-        buildAssetsDir:'/dist/',
+        // buildAssetsDir:'/dist/',
         // 設定讀取路徑
         cdnURL:'./',
       }, 
