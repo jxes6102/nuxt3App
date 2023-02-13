0c52500809f7220ada304469f3141e532005fcc3
jxes6102 <jxes6102@gamil.com>
2023-02-13 11:41:50 +0800

    temp02131145

--- pages/page8.vue
+++ pages/page8.vue
@@ -2,7 +2,7 @@
     <div>
         <div v-if="!dealStatus" class="mine-flex-center flex-col gap-5">
             <p class="mt-4 text-2xl font-bold text-gray-900">is page8 use ai test</p>
-            <input v-model="question" type="text" class="w-[60%] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="問題輸入">
+            <input v-model="question" type="text" class="w-[60%] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="問題輸入">
             <button @click="action" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">送出</button>
         </div>
         <div class="mine-flex-center flex-col gap-5 mt-2">
@@ -18,7 +18,7 @@ let configuration
 let openai:any
 const init = () => {
   configuration = new Configuration({
-    apiKey: "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",
+    apiKey: "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",
   })
   openai = new OpenAIApi(configuration)
   console.log('init ok',openai)
@@ -34,18 +34,21 @@ const action = async() => {
     question.value = ''
     return false
   }
-
+  /**
+   * model 模型
+   * prompt 問題主體
+   * temperature 回應隨機度
+   * max_tokens 問題字數限制
+   * frequency_penalty presence_penalty 重覆性懲罰
+   */
   try {
     dealStatus.value = true
     data.value = "思考中..."
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: question.value,
-      // prompt: "用繁體中文說明可樂",
-      // prompt: "翻譯test成繁體中文",
-      temperature: 0.7,
-      max_tokens: 256,
-      top_p: 1,
+      temperature: 0.6,
+      max_tokens: 512,
       frequency_penalty: 0,
       presence_penalty: 0,
     });
